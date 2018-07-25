48d366370a772254aa69c3864a2e8f61bf9be2b1
VINAY <VINAY@WIN-RP4BLDH6PHQ>
2018-07-25 10:11:07 +0530

    pattern commit

--- InterviewPractice/src/Pattern1.java
+++ InterviewPractice/src/Pattern1.java
@@ -14,11 +14,23 @@ public class Pattern1 {
          
         System.out.println("Here is your pattern....!!!");
          
-        for (int i = 1; i <= rows; i++) 
+        /*for (int i = 1; i <= rows; i++) 
         {
             for (int j = 1; j <= i; j++)
             {
                 System.out.print(j+" ");
+            }
+             
+            System.out.println();
+        }*/
+        
+        System.out.println("-----------------");
+        
+        for (int i = 1; i <= rows; i++) 
+        {
+            for (int j = 1; j <= i; j++)
+            {
+                System.out.print(i+" ");
             }
              
             System.out.println();
